d2563b9759b6292bb2507b5204dc525c1c29083c
Michael Williams <manicdesigner@gmail.com>
2018-04-15 20:58:21 -0400

    buttons  for listmanueverbuttons.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const listInputText = document.querySelector('.listInputText'); // data from input for list input.
 const addItemButton = document.querySelector('.addItemButton'); // button to add items to list.
 
+const ul = document.querySelector('.listOutputDiv');
 
 
 
@@ -10,6 +11,18 @@ const addItemButton = document.querySelector('.addItemButton'); // button to add
 
 
 
-const listmanuevButtons = (li) => {
+
+
+const listmanueverButtons = (li) => {
+   const up = createElement('button');
+   up.setAttribute('class', 'up');
+   up.textContent = 'Up';
    
+   const down = createElement('button');
+   down.setAttribute('class', 'down');
+   down.textContent = 'Down';
+
+   const remove = createElement('remove');
+   remove.setAttribute('class', 'remove');
+   remove.textContent = 'Remove';  
 };
