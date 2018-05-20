11bb86909354e144cc0bb85bd42c215e94932b4b
Michael Williams <manicdesigner@gmail.com>
2018-05-19 20:56:49 -0400

    had to add direct into addItem Button check button code to make it append.

--- js/main.js
+++ js/main.js
@@ -1,63 +1,49 @@
 
 const addItemButton = document.querySelector('button.addItemButton'); // button to add items to list.
 const hideItems = document.querySelector('button.hideItems'); // hide list button event handler.
-const listUl = document.querySelector('ul');
-
-
+const listUl = document.querySelector('ul'); 
 
 
 addItemButton.addEventListener('click', () => { // add items to list button.
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
-    li.setAttribute('class', 'move-items-js');
+    li.classList.add('move-items-js', 'text-center'); // add js drag class.
+
+    // mark done button..
+    const markDone = document.createElement('button');
+    markDone.setAttribute('class', 'complete btn');
+    markDone.textContent = 'Check';
     
+    // data input value placement before placing in list item.
     let listInputText = document.querySelector('.listInputText'); // data from input for list input. 
     li.textContent = listInputText.value; 
-    markDoneButton(li); // add done button.
-    ul.appendChild(li);
-    listInputText.value = '';
-  
+   
+    ul.append(li);
+    li.append(markDone);
+    listInputText.value = '';  
 });
-
 listUl.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
-       if (this.className === 'up btn') {
-           let listItem = event.target.parentNode;
-           let prevItem = listItem.previousElementSibling;
-           if (prevItem) {
-             listUl.insertBefore(listItem, prevItem);   
-           }      
-       } 
-       if (this.className === 'down btn') { // moves item down list.
-          let listItem = event.target.parentNode;
-          let nextItem = listItem.nextElementSibling;
-          if (nextItem) {
-            listUl.insertBefore(nextItem, listItem);
-          }
-          
-       }
-       if (event.target.className == 'done btn') { // remove items
+       if (event.target.className === 'complete btn') { // remove items
            const listItem = document.querySelector('.move-items-js');
            let b = listItem.textContent;
 
-           const button = document.querySelector('button.done');
+           const button = document.querySelector('button.complete');
            if(button) {
           
            let c = button.textContent;
            console.log(c); // button only console test.
-           }
-           console.log(b); // text and button console log.       
-       }
+           } 
+        } 
+        if (event.target.className === 'move-items-js') {
+          let i = document.querySelector('li');
+          let text = i.textContent;
+          console.log(text);
+        }
+       
+       
     }
 });
-
-const markDoneButton = (li) => {
-    const remove = document.createElement('button');
-    remove.setAttribute('class', 'done btn');
-    remove.textContent = 'Done';
-    li.append(remove);
- 
- };
 
  hideItems.addEventListener('click', () => { // hide entire list and board.
     let listInputOutput = document.querySelector('.listInputOutput'); // entire board section.
@@ -65,6 +51,5 @@ const markDoneButton = (li) => {
         listInputOutput.style.display = 'none';    // then style equals the second(block) not the first(none).
     } else {
         listInputOutput.style.display = 'block';
-    }
-    
+    }    
 });
